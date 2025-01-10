1772679232374a2e9dce10678b6b84744dedeca7
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2025-01-10 14:36:01 -0800

    mv `baseModules` to `base.nix`

---
+++ base.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nix.settings.experimental-features = [ "nix-command" "flakes" "recursive-nix" ];
+
+  networking = {
+    networkmanager.enable = true;
+    wireless.enable = lib.mkForce false;
+    # ^because WPA Supplicant cannot run with NetworkManager
+  };
+
+  environment.systemPackages = with pkgs; [
+    age agenix bash curl git neovim tmux zsh
+  ];
+}
--- flake.nix
+++ flake.nix
@@ -5,53 +5,26 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    rust-overlay.url = "github:oxalica/rust-overlay";
-
-    agenix.url = "github:yaxitech/ragenix";
-    agenix.inputs.nixpkgs.follows = "nixpkgs";
-
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = { nixpkgs, rust-overlay, agenix, home-manager, ... }:
-  let
-
-    lib = nixpkgs.lib;
-    system = "x86_64-linux";
-    overlays = [ rust-overlay.overlays.default agenix.overlays.default ];
-
-    pkgs = import nixpkgs {
-      inherit system overlays;
-      config.allowUnfree = true;
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    baseModules = [
-      agenix.nixosModules.age
-      ({ lib, pkgs, ... }: {
-        nix = {
-          package = pkgs.nixUnstable;
-          extraOptions = "experimental-features = nix-command flakes recursive-nix";
-        };
+    agenix = {
+      url = "github:yaxitech/ragenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-        networking = {
-          networkmanager.enable = true;
-          wireless.enable = lib.mkForce false;
-          # ^because WPA Supplicant cannot run with NetworkManager
-        };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-        environment.systemPackages = with pkgs; [
-          age agenix
-          curl git neovim tmux
-        ];
-      })
-    ];
-
-  in {
+  outputs = { self, nixpkgs, home-manager, sops-nix, ... }@inputs : {
 
     homeConfigurations = {
       flynn = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${system};
         modules = [ ./user/flynn/home.nix ];
         # extraSpecialArgs = { inherit inputs outputs; };
       };
@@ -59,21 +32,23 @@
 
     nixosConfigurations = {
 
-      iso = lib.nixosSystem {
-        inherit pkgs system;
-        modules = baseModules ++ [
+      iso = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./base.nix
           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
         ];
       };
 
-      encom = lib.nixosSystem {
-        inherit pkgs system;
-        modules = baseModules ++ [
-            home-manager.nixosModules.home-manager {
+      encom = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             # ^otherwise pure evaluation fails for flakes
             home-manager.useUserPackages = true;
           }
+          ./base.nix
           ./os/encom
           ./user/flynn
         ];
