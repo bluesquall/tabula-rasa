63ef79d08378ddcefdd13859f814bcf8e12c3bc5
M J Stanway <m.j.stanway@alum.mit.edu>
2025-01-10 19:42:54 -0800

    return to LUKS and prototype YubiKey integration
    following github.com/sgillespie/nixos-yubikey-luks

--- os/filesystems.nix
+++ os/filesystems.nix
@@ -5,13 +5,34 @@
 {
   boot.kernelParams = [ "nohibernate" ];
   boot.initrd = {
-    availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+    availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" "vfat" "nls_cp437" "nls_iso8859-1" ];
     postDeviceCommands = lib.mkAfter ''
       zfs rollback -r rpool/transient/root@strap
     '';
     supportedFilesystems = [ "zfs" ];
   };
   boot.supportedFilesystems = [ "zfs" ];
+
+  luks = {
+    yubikeySupport = true
+    devices."ZED" = {
+      device = "/dev/disk/by-partlabel/LUKS";
+
+      yubikey = {
+        slot = 2;
+	twoFactor = false;
+	gracePeriod = 30;
+	keyLength = 64;
+	saltLength = 16;
+
+	storage = {
+          device = "/dev/disk/by-partlabel/EFI";
+	  fstype = "vfat";
+	  path = "/salt-it";
+	};
+      };
+    };
+  };
 
   fileSystems = {
     "/" = { 
